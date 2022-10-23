a75b964479509cf8f31f150ef5e95c32a6e6ab53
Sonia Orive <isavo70@hotmail.com>
2022-10-23 19:38:57 +0200

    Customise navbar and deploy navbar-burguer

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-
-    <?php include "./inc/head.php";?>
-    
-</head>
-
-<body>
-    <?php include "./inc/navbar.php";?>
-    
-        
-</body>
+    <head>
+        <?php include "./inc/head.php";?>  
+    </head>
+    <body>
+    <!-- navbar -->
+        <?php include "./inc/navbar.php";?>  
+    <!-- script navbar-burger -->
+        <?php include "./inc/script.php";?>
+    </body>
 
 </html>
