16cd3211b9d9a1a3a14ab6e10c54765d36bcb025
Sonia Orive <isavo70@hotmail.com>
2022-10-23 20:51:57 +0200

    Create structure and style 404

--- vistas/404.php
+++ vistas/404.php
@@ -1,1 +1,15 @@
-404
+<div class="main-container">
+    <section class="hero-body">
+        <div class="hero-body">
+            <p class="title">
+                Error 404
+            </p>
+            <p class="subtitle">
+                Página no encontrada
+            </p>
+            <p class="has-text-centered mb-4 mt-3">
+                <button type="button" class="button is-primary is-medium is-rounded"><a href="../index.php">Volver</a></button>
+            </p>
+        </div>
+    </section>
+</div>
