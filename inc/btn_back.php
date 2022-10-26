7500af3c8657c87e45e44d124bd2cfbd5a1b6658
Sonia Orive <isavo70@hotmail.com>
2022-10-26 21:07:07 +0200

    Load category data into the form

--- inc/btn_back.php
+++ inc/btn_back.php
@@ -1,6 +1,6 @@
  <p class="has-text-right pt-4 pb-4">
      <a href="#" class="button is-link is-rounded btn-back">
-         < Anterior</a>
+         < Atrás</a>
  </p>
  <script type="text/javascript">
      let btn_back = document.querySelector(".btn-back");
